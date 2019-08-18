a6f0f83a3b4b201c36525fd58e15e67fbfbe086c
galekseev <alekseev.gleb@gmail.com>
2019-08-18 23:10:10 +0300

    Fixed release config

---
+++ app/src/release/java/com/camomile/openlibre/StethoUtils.java
@@ -0,0 +1,12 @@
+package com.camomile.openlibre;
+
+import android.app.Application;
+import java.io.File;
+
+public class StethoUtils {
+
+   public static void install(Application application, File dataPath){
+        // no debugging
+   }
+}
+
